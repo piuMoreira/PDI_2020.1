f1be17bfa4c818337a86467fbd0b0e4ac7dcf1d5
Renan Moreira <piumoreiras@gmail.com>
2020-11-15 16:07:41 -0300

    melhora de funções

--- rgb2yiq.py
+++ rgb2yiq.py
@@ -1,7 +1,7 @@
 import numpy as np
 import loader
 
-def rgb2yiq(img_in):
+def rgb2yiq(img_in : np.array):
 	mat_yiq = np.array(
         [[0.299, 0.587, 0.114], [0.59590059, -0.27455667, -0.32134392], [0.21153661, -0.52273617, 0.31119955]])
 	img_out = np.copy(img_in).astype(np.float32)
@@ -15,7 +15,7 @@ def rgb2yiq(img_in):
 			
 	return img_out
 
-def yiq2rgb(img_in):
+def yiq2rgb(img_in : np.array):
 	mat_rgb = np.array(
         [[1, 0.956, 0.621], [1, -0.272, -0.647], [1, -1.106, 1.703]])
 	img_out = np.copy(img_in)
@@ -28,8 +28,3 @@ def yiq2rgb(img_in):
 			img_out[i, j] = pixelrgb
 	
 	return img_out.astype(np.uint8)
-
-img_in = loader.open_image('./images/baboon.png')
-img_yiq = rgb2yiq(img_in)
-img_rgb = yiq2rgb(img_yiq)
-loader.save_image(img_rgb, './baboon-rgb2yiq-yiq2rgb.png')
